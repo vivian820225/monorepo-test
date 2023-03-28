a061a7b05421c9ba4ee59b188cd728eff60e8871
VivianChiang <vivian820225@gmail.com>
2023-03-28 18:15:43 +0800

    add: common-components
    pnpm --filter project-1 add common-components --workspace

--- apps/project-1/src/main.js
+++ apps/project-1/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import commonUI from "common-components";
+import "common-components/dist/index.css";
 
-createApp(App).mount('#app')
+createApp(App).use(commonUI).mount("#app");
